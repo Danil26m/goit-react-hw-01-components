6a03c55b9bf52f15cbcca02420f708b6ca56fc06
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-04 19:43:21 +0200

    danil

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,20 @@ import Statistics from "./Statistics/Statistics.js";
 import data from "./Statistics/data.json"
 import FriendList from "./FriendList/FriendList.js";
 import friends from "./FriendList/friends.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory.js";
+import transactions from "./TransactionHistory/transactions.json";
 export const App = () => {
   const color = ['aqua','rgb(91, 78, 177)','rgb(197, 80, 80)','rgb(80, 197, 164)','rgb(183, 78, 125)'];
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        padding: '20px 0'
       }}
     >
       <Profile
@@ -33,6 +35,9 @@ export const App = () => {
       />
       <FriendList
         friends={friends}
+      />
+      <TransactionHistory 
+        items={transactions} 
       />
     </div>
   );
